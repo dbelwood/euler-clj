8b91efcdbca94de2c38944ad78e194368c264ab7
Dan Belwood <dan.belwood@gmail.com>
2016-01-03 21:59:27 -0600

    Add efficient gcd formula

--- src/euler/utils.clj
+++ src/euler/utils.clj
@@ -1,5 +1,6 @@
 (ns euler.utils)
 
+;; Good sequences to have
 (def odd-seq
   (iterate (partial + 2) 1))
 
@@ -18,4 +19,16 @@
                 (filter prime?
                         odd-seq))))
 
+;; Mathematical algorithms
+(defn gcd [a b]
+  (if (> b a)
+    (gcd b a)
+    (let [a (rem a b)]
+      (loop [a a b b]
+        (let [r (rem a b)]
+          (if (zero? r)
+            b
+            (recur b r)))))))
+
+;; Useful transformers
 (defn char->int [x] (- (int x) 48))
